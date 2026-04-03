f7835c26c50f549a6e3c38ab2c94b12fe5b96a08
softgenai <admin@softgen.ai>
2026-04-03 18:10:36 +0000

    feat(assets): update favicon to use logo jpg

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,36 +16,16 @@ export default function Document() {
         <meta name="msapplication-navbutton-color" content="#1e3a8a" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         
-        {/* Apple Touch Icons */}
-        <link rel="apple-touch-icon" href="/icon-152.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/icon-72.png" />
-        <link rel="apple-touch-icon" sizes="96x96" href="/icon-96.png" />
-        <link rel="apple-touch-icon" sizes="128x128" href="/icon-128.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/icon-144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icon-152.png" />
-        <link rel="apple-touch-icon" sizes="192x192" href="/icon-192.png" />
-        <link rel="apple-touch-icon" sizes="384x384" href="/icon-384.png" />
-        <link rel="apple-touch-icon" sizes="512x512" href="/icon-512.png" />
+        {/* Favicons - Updated to use new logo */}
+        <link rel="icon" type="image/jpeg" href="/harding-homes-logo.jpg" />
+        <link rel="shortcut icon" href="/harding-homes-logo.jpg" />
         
-        {/* Apple Splash Screens */}
-        <link rel="apple-touch-startup-image" href="/splash-2048x2732.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="/splash-1668x2388.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="/splash-1536x2048.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="/splash-1242x2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="/splash-828x1792.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="/splash-1242x2208.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+        {/* Apple Touch Icons - Will be updated with proper PNG icons */}
+        <link rel="apple-touch-icon" href="/harding-homes-logo.jpg" />
         
         {/* Apple Web App */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Harding Homes" />
-        
-        {/* Favicons */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/icon-32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icon-16.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <body className="antialiased">
         <Main />
